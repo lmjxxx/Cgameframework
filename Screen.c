29e82017db931395e4780db0bfe1f26c3e4fe2ea
lmxx <limjx20606@kakao.com>
2023-02-09 14:56:09 +0900

    a

--- Screen.c
+++ Screen.c
@@ -1,5 +1,6 @@
 
 #include "Screen.h"
+#include <ncurses.h>
 
 /*
 void ScreenInit();
@@ -11,6 +12,7 @@ void SetColor(unsigned short color);
 https://m.cafe.daum.net/nowcampus/dZlN/5?listURI=%2Fnowcampus%2FdZlN
 https://huiyu.tistory.com/entry/C언어로-게임만들기-3-렌더링-기능-구현
 https://github.com/brenns10/tetris/blob/master/src/main.c
+https://m.blog.naver.com/guile21c/52637374
 
 */
 
@@ -21,9 +23,9 @@ windows.h 를 못쓰니 ncurses를 사용해서 구현해야한다.
 */
 void ScreenInit()
 {
-    
+    initscr(); // 커스 터미널 초기화 코드, 화면을 지우고 공백 상태로 만든다.
+    keypad();
 }
-
 /*
 화면 버퍼 지우기
 하나의 화면 버퍼가 활성화되어 출력되고 있는 동안에 다음 장면을 위한 화면 버퍼는 지워야 한다.
@@ -40,6 +42,7 @@ void ScreenClear()
 */
 void ScreenFlipping()
 {
+    refresh(); // 가상의 윈도우 버퍼상에 문자열을 출력하여 refresh() 함수가 실행 되었을 때 그 내용이 화면상에 출력하게 된다. 
 }
 
 /*
@@ -48,6 +51,7 @@ void ScreenFlipping()
 */
 void ScreenRelease()
 {
+    endwin(); //ncurse 사용 동적메모리를 반납하고 터미널을 다시 일반 모드로 전환시킨다. 
 }
 
 /*
@@ -56,13 +60,33 @@ void ScreenRelease()
 */
 void ScreenPrint(int x, int y, char *string)
 {
+    //printw("ㅁ"); // printf와 동일한 기능, 현재의 커서 위치(x,y) 좌표상에 문자열을 출력해준다. 문자열 출력 버퍼상에 해당 문자들이 채워지고 이후의 refresh() 함수에 의해 실제 화면상에 표시하게 된다.
+    //mvprintw(y,x,"a"); // printw에 좌표지정 기능이 표함된 함수
+    //move(y, x); //현재으 ㅣ커서 위치를 내가 원하는 행(ROW, y)과 열(COL,x) 위치로 이동시키낟. addstr mvaddstr
+    //getmaxyx() 현재 터미널의 가로, 세로 크기를 얻어온다. 
 }
 
 /*
 출력 문자의 색상을 설정하는 함수
-출력 문자의 색상값은 1에서부터 15까지 설정할 수 있으며 다음 SecColor() 함수가 사용될 때까지 이전에 사용한 SetColor()의 색상이 모든 문자에 적용된다.
+출력 문자의 색상값은 0에서부터 7까지 설정할 수 있으며 다음 SecColor() 함수가 사용될 때까지 이전에 사용한 SetColor()의 색상이 모든 문자에 적용된다.
 이 색상은 대기 화면 버퍼에 적용된다.
+#define COLOR_BLACK	0
+#define COLOR_RED	1
+#define COLOR_GREEN	2
+#define COLOR_YELLOW	3
+#define COLOR_BLUE	4
+#define COLOR_MAGENTA	5
+#define COLOR_CYAN	6
+#define COLOR_WHITE	7
 */
 void SecColor(unsigned short color)
 {
+    if (has_colors() == FALSE) // has_colors 현재의 단말기가 컬러를 지원하는지 유무 확인,
+    {
+        endwin();
+        printf("Your terminal does not support color \n");
+        exit(1);
+    }
+    start_color();                           // 컬러 폰트를 사용 하기에 앞서 항상 해줘야 하는 초기화 함수
+    init_pair(1, COLOR_BLACK, COLOR_YELLOW); // init_pair(팔래트 넘버, 폰트 전경색, 배경색);
 }
--- Screen.h
+++ Screen.h
@@ -7,13 +7,16 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
+static int nScreenIndex; //버퍼의 Index를 나타낼 변수
+static char * hScreen[2]; //전위버퍼와 후위버퍼
+static int HEIGHT, WIDTH; // 그릴 화면의 사이즈
+
 void ScreenInit(); 
 void ScreenFlipping();
 void ScreenClear();
 void ScreenRelease();
 void ScreenPrint(int x, int y, char * string);
 void SetColor(unsigned short color);
-
 
 
 #endif //SCREEN_H
