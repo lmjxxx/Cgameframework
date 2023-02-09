97eb379a54a8ee8951010d17ecb229a2d8efb51f
lmxx <limjx20606@kakao.com>
2023-02-09 14:52:50 +0900

    renew

--- framework.c
+++ framework.c
@@ -1,12 +1,22 @@
 #include "framework.h"
-#include "Screen.h"
-#include "key.h"
-#include "InitialSet.h"
 
+/*
+savetty: 현재의 터미널 상태 정보를 저장(backup)한다.
+resetty: 현재의 터미널 상태 정보를 복원(restore)한다.
+*/
+void termInit()
+{
+    savetty();
+}
+/*
+initscr: 커스 터미널을 초기화하는 함수.
+keypad: 방향키, f1 f2 등 특수키를 사용할 수 있도록 초기화 하는 함수
+*/
 void Init()
 {
-    initscr(); // initialize curses
-};
+    initscr(); // curse terminal initializing
+    keypad(stdscr, TRUE);
+}
 
 void Render()
 {
@@ -16,6 +26,15 @@ void Update()
 {
 }
 
+/*
+터미널 상태 정보를 복원한다.
+*/
 void Release()
 {
+    resetty();
+}
+
+void PrintScreen(int x, int y, char *string)
+{
+    mvprinttw(y, x, string);
 }
--- framework.h
+++ framework.h
@@ -2,13 +2,38 @@
     NAME: framework.h
     DESCRIPTION: 게임 제작에 필요한 기능들을 다 모아놓음.
     AUTHOR: lmxx
+    https://m.cafe.daum.net/nowcampus/dZlN/5?listURI=%2Fnowcampus%2FdZlN
+    https://huiyu.tistory.com/entry/C언어로-게임만들기-3-렌더링-기능-구현
+    https://github.com/brenns10/tetris/blob/master/src/main.c
+    https://m.blog.naver.com/guile21c/52637374
 */
 
 #ifndef FRAMEWORK_H
+#define FRAMEWORK_H
 
-void Init();    // 초기화
-void Render();  // 화면 출력
-void Update();  // 데이터 갱신
-void Release(); // 해제
+#include <stdio.h>
+#include <ncurses.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+typedef struct Character
+{
+    char *sprite;
+    int char_x_size;
+    int char_y_size;
+    short x;
+    bool collider;
+} character;
+
+void Init();     // 초기화
+void termInit(); // 터미널 초기화
+void Render();   // 화면 출력
+void Update();   // 데이터 갱신
+void Release();  // 해제
+
+void PrintScreen(int x, int y, char *string);
+void SetColor(unsigned short color);
+void moving(int x, int y);
+void drawing_box(int x, int y, unsigned short color);
 
 #endif // FRAMEWORK_H
